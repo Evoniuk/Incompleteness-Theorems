5c719148e65ab493e2e1edd067800110c167dfda
Jack Evoniuk <45554423+Evoniuk@users.noreply.github.com>
2019-05-16 11:40:16 -0700

    Update Godel.py

--- Godel.py
+++ Godel.py
@@ -1,9 +1,9 @@
-def godel(x):
-    number = {'0':1,'f':3,'~':5,'v':7,'a':9,'(':11,')':13,'X':17,'Y':289}
+def godel(string):
+    mapping = {'0':1,'f':3,'~':5,'v':7,'a':9,'(':11,')':13,'X':17,'Y':289}
     primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61]
-    z = 1
+    result = 1
     index = 0
-    for character in x:
-        z = z*primes[index]**number[character]
+    for character in string:
+        result *= primes[index]**mapping[character]
         index += 1
-    return z
+    return result
